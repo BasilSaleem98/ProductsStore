63a114fb8cd47876503a69996da3ab531734ca68
BasilSaleem98 <basilsom1998@gmail.com>
2026-06-06 11:02:41 +0300

    upload TC_UI_I002

--- src/main/java/SoloProject/ProductsStore/homePage.java
+++ src/main/java/SoloProject/ProductsStore/homePage.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class homePage {
 	WebDriver driver;						
 	
-								//locators for UI TCs
+											//locators for UI TCs
 	// "about us" locator in downbar 
     By aboutUsDownBar = By.xpath("//b[normalize-space()='About Us']");
     // "about us" paragraph locator in downbar 
@@ -22,7 +22,13 @@ public class homePage {
     // "product store" locator in downbar 
     By productStoreDownBar = By.xpath("//h4[normalize-space()='PRODUCT STORE']");
     By sonyVaioI5Vis = By.xpath("//a[normalize-space()='Sony vaio i5']");
-  
+  ///*****************************************
+    //locator for next page button
+    By nextPageBtn = By.xpath("//button[@id='next2']");
+    //locator for apple monitor product
+    By appleMonitor24 = By.xpath("//a[normalize-space()='Apple monitor 24']");
+    //locator for apple monitor price
+    By appleMonitor24Price = By.xpath("//h5[normalize-space()='$400']");
     
     //////////////////////////////////////////////////TC_UI_I001
     
@@ -53,9 +59,25 @@ public class homePage {
     public boolean isProductStoreVis() {
 		return driver.findElement(productStoreDownBar).isDisplayed();
 	}
-    
-    
-    
+    //***********************************************************
+    //click on next page button
+    public void clickNextPage() {
+		driver.findElement(nextPageBtn).click();
+	}
+    //vrify that apple monitor is visible 
+    public String getAppleMonitor24Vis() {
+    	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); //wait till sony i5 is visible 
+		WebElement apple = wait.until(ExpectedConditions.refreshed(ExpectedConditions.elementToBeClickable(appleMonitor24)));
+		return apple.getText();
+		
+	}
+  //vrify that apple monitor price is visible 
+    public String getAppleMonitor24PriceVis() {
+    	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); //wait till sony i5 is visible 
+		WebElement apple = wait.until(ExpectedConditions.refreshed(ExpectedConditions.elementToBeClickable(appleMonitor24Price)));
+		return apple.getText();
+	}
+    //**************************************************************
     
     
     
--- src/test/java/SoloProject/ProductsStore/homeTest.java
+++ src/test/java/SoloProject/ProductsStore/homeTest.java
@@ -7,7 +7,7 @@ public class homeTest extends baseTest {
 	
 							//TC_UI_I001 Scroll down and verify that footer is visible	
 		@Test	
-		public void scrollVerifyThatFooterIsVisable	() {
+		public void scrollVerifyThatFooterIsVisible	() {
 			
 			homePage home = new homePage(driver);
 			home.scrollDown();
@@ -19,9 +19,20 @@ public class homeTest extends baseTest {
 			Assert.assertEquals(home.isGetInTouchVis(), true, " Get in touch not visible ");
 		    // verify that "product store" in down bar visible 
 			Assert.assertEquals(home.isProductStoreVis(), true, " Product store not visible ");
-			
 		}
-	
+		
+						//TC_UI_I001 Scroll down and switch to another page	
+		@Test
+		public void switchToNextPage() {
+			homePage home = new homePage(driver);
+			home.scrollDown();
+			home.clickNextPage();
+		    //verify that apple monitor visible 
+			Assert.assertEquals(home.getAppleMonitor24Vis(), "Apple monitor 24", " The page didnt switch and apple monitor not visible ");
+		    //verify that apple monitor price visible 
+			Assert.assertEquals(home.getAppleMonitor24PriceVis(), "$400", " The page didnt switch and apple monitor price not visible ");
+		}
+		
 	
 
 }
